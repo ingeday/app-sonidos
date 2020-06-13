ac40f27f0310ebcb4bd1475ab7082e4f7ac1fd7e
Eday Gonzalez <edaygonzalez@ingenieros.com>
2020-06-13 17:46:45 -0500

    Aplicacion terminado de sonidos de animales del curso de Fernando Herrera

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Animal } from './../../../../sonidos/src/interfaces/animal.interface';
 import { ANIMALES } from './../../data/data.animales';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Refresher, reorderArray } from 'ionic-angular';
 
 
 @Component({
@@ -11,19 +11,58 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   animales:Animal[];
+  audio = new Audio();
+  audioTiempo: any;
+  ordering:boolean = false;
+
   constructor(public navCtrl: NavController) {
-    this.animales = ANIMALES.splice(0);
+    this.animales = ANIMALES.slice();
     
   }
 
   play(animal:Animal) {
-    let audio = new Audio();
-    audio.src = animal.audio;
-    audio.load();
-    audio.play();
+    
+    this.pause(animal);
+
+    if(animal.reproduciendo) {
+     animal.reproduciendo = false;
+     return;
+    }
+
+    this.audio.src = animal.audio;
+    this.audio.load();
+    this.audio.play();
     animal.reproduciendo = true;
 
-    setTimeout(()=> animal.reproduciendo=false, animal.duracion*1000);
+    this.audioTiempo = setTimeout(()=> animal.reproduciendo=false, animal.duracion*1000);
+  }
+
+  private pause(animalSelected:Animal) {
+    clearTimeout( this.audioTiempo );
+    this.audio.pause();
+    this.audio.currentTime = 0;
+
+    for(let animal of this.animales) {
+      if(animal.nombre != animalSelected.nombre) {
+        animal.reproduciendo = false;
+      }
+    }
+  }
+
+  remove(index:number) {
+    this.animales.splice(index, 1)
+  }
+
+  doRefresh(refresher:Refresher) {
+    console.log('Begin async operation', refresher);
+    setTimeout(() => {
+      this.animales = ANIMALES.slice();
+      refresher.complete();
+    }, 2000);
+  }
+
+  reOrder(reorder:any) {
+    this.animales = reorderArray(this.animales, reorder);
   }
 
 }
